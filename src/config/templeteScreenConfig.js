23746d8c5a63dff452c84a7a42fea75aa7637376
satishdonerao <doneraosatish@gmail.com>
2025-01-29 14:01:16 +0530

    config template chnages

--- src/config/templeteScreenConfig.js
+++ src/config/templeteScreenConfig.js
@@ -3,12 +3,6 @@ export const TEMPLET_SCREEN_CONFIG = {
     key: "numberS2NTemplate",
     header: "Match Sound To Number",
     columns: [
-      // {
-      //   Header: "ID",
-      //   accessor: "id",
-      //   type: "string",
-      // },
-      
       {
         Header: "English Question",
         accessor: "engQuestion",
@@ -22,13 +16,13 @@ export const TEMPLET_SCREEN_CONFIG = {
       {
         Header: "Options",
         accessor: "options",
-        type: "json", // For JSON data
+        type: "json",
         Cell: ({ value }) => renderJsonCell(value, 'array')
       },
       {
         Header: "Answers",
         accessor: "answers",
-        type: "json", // For JSON data
+        type: "json",
         Cell: ({ value }) => renderJsonCell(value, 'array')
       },
       {
@@ -46,16 +40,6 @@ export const TEMPLET_SCREEN_CONFIG = {
         accessor: "Grade.name",
         type: "string",
       },
-      // {
-      //   Header: "Created At",
-      //   accessor: "createdAt",
-      //   type: "string",
-      // },
-      // {
-      //   Header: "Updated At",
-      //   accessor: "updatedAt",
-      //   type: "string",
-      // },
     ],
     importSampleFile:
       "https://docs.google.com/spreadsheets/d/1BXPXNDXQBD57MWlNjKDrTXHpMF2tbbfCKRJogwT4VF8/edit?usp=drive_link",
@@ -70,111 +54,422 @@ export const TEMPLET_SCREEN_CONFIG = {
         accessor: "aa",
         type: "string",
       },
+      {
+        Header: "Options",
+        accessor: "options",
+        type: "json",
+        Cell: ({ value }) => renderJsonCell(value, 'array'),
+      },
+      {
+        Header: "Answers",
+        accessor: "answers",
+        type: "json",
+        Cell: ({ value }) => renderJsonCell(value, 'array'),
+      },
+      {
+        Header: "University",
+        accessor: "University.name",
+        type: "string",
+      },
+      {
+        Header: "Subject",
+        accessor: "Subject.name",
+        type: "string",
+      },
+      {
+        Header: "Grade",
+        accessor: "Grade.name",
+        type: "string",
+      },
     ],
     importSampleFile:
       "https://docs.google.com/spreadsheets/d/1TtaDpuFBCmeadlBcp2BHeQRw8bP2LCTIsEj5ZJeV-mE/edit?gid=0#gid=0",
     icon: "rule",
   },
+
   soundToImageTemplate: {
     key: "soundToImageTemplate",
     header: "Sound To Image",
     columns: [
       {
-        Header: "Question",
-        accessor: "aa",
+        Header: "English Question",
+        accessor: "engQuestion",
+        type: "string",
+      },
+      {
+        Header: "Hindi Question",
+        accessor: "hinQuestion",
+        type: "string",
+      },
+      {
+        Header: "Options",
+        accessor: "options",
+        type: "json",
+        Cell: ({ value }) => renderJsonCell(value, 'array'),
+      },
+      {
+        Header: "Answers",
+        accessor: "answers",
+        type: "json",
+        Cell: ({ value }) => renderJsonCell(value, 'array'),
+      },
+      {
+        Header: "University",
+        accessor: "University.name",
+        type: "string",
+      },
+      {
+        Header: "Subject",
+        accessor: "Subject.name",
+        type: "string",
+      },
+      {
+        Header: "Grade",
+        accessor: "Grade.name",
         type: "string",
       },
     ],
     importSampleFile:
       "https://docs.google.com/spreadsheets/d/1cKvnLa6MG1wRdr0zIDf9Tu5Ze_5ngeEV4fWXH-UDfbc/edit?gid=0#gid=0",
     icon: "rule",
+    pagination: {
+      totalRecords: 24,
+      currentPage: 5,
+      totalPages: 5,
+    },
+    data: [],
   },
+
   numberReOrderTemplates: {
     key: "numberReOrderTemplates",
     header: "Number Reorder",
     columns: [
       {
         Header: "Question",
-        accessor: "aa",
+        accessor: "question",
+        type: "string",
+      },
+      {
+        Header: "Options",
+        accessor: "options",
+        type: "json",
+        Cell: ({ value }) => renderJsonCell(value, 'array'),
+      },
+      {
+        Header: "Answer",
+        accessor: "answer",
+        type: "json",
+        Cell: ({ value }) => renderJsonCell(value, 'array'),
+      },
+      {
+        Header: "University",
+        accessor: "University.name",
+        type: "string",
+      },
+      {
+        Header: "Subject",
+        accessor: "Subject.name",
+        type: "string",
+      },
+      {
+        Header: "Grade",
+        accessor: "Grade.name",
         type: "string",
       },
     ],
     importSampleFile:
       "https://docs.google.com/spreadsheets/d/1PQkCMjD7TAW3W4tfsqk0lHWZ4gtJhvK0PNBi0jQ4Bew/edit?gid=0#gid=0",
     icon: "rule",
+    pagination: {
+      totalRecords: 28,
+      currentPage: 5,
+      totalPages: 6,
+    },
+    data: [],
   },
+
   numberAscDesTemplate: {
     key: "numberAscDesTemplate",
     header: "Number ASC DES",
     columns: [
       {
         Header: "Question",
-        accessor: "aa",
+        accessor: "question",
+        type: "json",
+        Cell: ({ value }) => renderJsonCell(value, 'array'),
+      },
+      {
+        Header: "Answer",
+        accessor: "answer",
+        type: "json",
+        Cell: ({ value }) => renderJsonCell(value, 'array'),
+      },
+      {
+        Header: "English Title",
+        accessor: "engTitle",
+        type: "string",
+      },
+      {
+        Header: "Hindi Title",
+        accessor: "hinTitle",
+        type: "string",
+      },
+      {
+        Header: "University",
+        accessor: "University.name",
+        type: "string",
+      },
+      {
+        Header: "Subject",
+        accessor: "Subject.name",
+        type: "string",
+      },
+      {
+        Header: "Grade",
+        accessor: "Grade.name",
         type: "string",
       },
     ],
     importSampleFile:
       "https://docs.google.com/spreadsheets/d/1ArrSBpBV4p-fpHT10AZu8sS6mpqOpcOlvaDuTmg_F-I/edit?gid=0#gid=0",
     icon: "rule",
+    pagination: {
+      totalRecords: 60,
+      currentPage: 5,
+      totalPages: 12,
+    },
+    data: [],
   },
+
   learningFillNumberTemplate: {
     key: "learningFillNumberTemplate",
     header: "Number ASC DES",
     columns: [
       {
         Header: "Question",
-        accessor: "aa",
+        accessor: "question",
+        type: "json",
+        Cell: ({ value }) => renderJsonCell(value, 'array'),
+      },
+      {
+        Header: "Answer",
+        accessor: "answer",
+        type: "json",
+        Cell: ({ value }) => renderJsonCell(value, 'array'),
+      },
+      {
+        Header: "English Title",
+        accessor: "engTitle",
+        type: "string",
+      },
+      {
+        Header: "Hindi Title",
+        accessor: "hinTitle",
+        type: "string",
+      },
+      {
+        Header: "University",
+        accessor: "University.name",
+        type: "string",
+      },
+      {
+        Header: "Subject",
+        accessor: "Subject.name",
+        type: "string",
+      },
+      {
+        Header: "Grade",
+        accessor: "Grade.name",
         type: "string",
       },
     ],
     importSampleFile:
       "https://docs.google.com/spreadsheets/d/1aTcDhkxNi1juwfBR8QIkXwy49MjhDa3RIFlo1Xnp-qQ/edit?gid=0#gid=0",
     icon: "rule",
+    pagination: {
+      totalRecords: 30,
+      currentPage: 5,
+      totalPages: 6,
+    },
+    data: [],
   },
+
   learningNumberTemplate: {
     key: "learningNumberTemplate",
     header: "Learning Number",
     columns: [
       {
-        Header: "Question",
-        accessor: "aa",
+        Header: "Number",
+        accessor: "number",
+        type: "string",
+      },
+      {
+        Header: "English Number",
+        accessor: "engNumber",
+        type: "string",
+      },
+      {
+        Header: "Hindi Phonetics",
+        accessor: "hinPhonetics",
+        type: "string",
+      },
+      {
+        Header: "English Translation",
+        accessor: "engTranslation",
+        type: "string",
+      },
+      {
+        Header: "English Word",
+        accessor: "engWord",
+        type: "string",
+      },
+      {
+        Header: "Hindi Word",
+        accessor: "hinWord",
+        type: "string",
+      },
+      {
+        Header: "Image",
+        accessor: "image",
+        type: "string",
+        Cell: ({ value }) => <img src={value} alt="Number image" style={{ width: 50, height: 50 }} />,
+      },
+      {
+        Header: "University",
+        accessor: "University.name",
+        type: "string",
+      },
+      {
+        Header: "Subject",
+        accessor: "Subject.name",
+        type: "string",
+      },
+      {
+        Header: "Grade",
+        accessor: "Grade.name",
         type: "string",
       },
     ],
     importSampleFile:
       "https://docs.google.com/spreadsheets/d/1vnXk01GK5DMDMn4jVYB7I_yL-eVkkwN1f5z7mBUkh1w/edit?gid=0#gid=0",
     icon: "rule",
+    pagination: {
+      totalRecords: 30,
+      currentPage: 5,
+      totalPages: 6,
+    },
+    data: [],
   },
+
   learningFillAplTemplate: {
     key: "learningFillAplTemplate",
     header: "Fill Alphabet",
     columns: [
       {
-        Header: "Question",
-        accessor: "aa",
+        Header: "English Title",
+        accessor: "engTitle",
         type: "string",
+      },
+      {
+        Header: "Hindi Title",
+        accessor: "hinTitle",
+        type: "string",
+      },
+      {
+        Header: "Questions",
+        accessor: "questions",
+        type: "array",
+        Cell: ({ value }) => value.join(" -> "),
+      },
+      {
+        Header: "Options",
+        accessor: "options",
+        type: "array",
+        Cell: ({ value }) => value.join(", "),
+      },
+      {
+        Header: "University",
+        accessor: "University.name",
+        type: "string",
+        Cell: ({ value }) => value || "N/A",
+      },
+      {
+        Header: "Subject",
+        accessor: "Subject.name",
+        type: "string",
+        Cell: ({ value }) => value || "N/A",
+      },
+      {
+        Header: "Grade",
+        accessor: "Grade.name",
+        type: "string",
+        Cell: ({ value }) => value || "N/A",
       },
     ],
     importSampleFile:
       "https://docs.google.com/spreadsheets/d/1jkkU9Cl7s7-iuHIlLBR2C4xe8o5gnXqkYJ-r4REORGA/edit?gid=0#gid=0",
     icon: "rule",
+    pagination: {
+      totalRecords: 28,
+      currentPage: 5,
+      totalPages: 6,
+    }
   },
+
+
   learningW2WTemplate: {
     key: "learningW2WTemplate",
-    header: "Alphabet Lettor to Letter",
+    header: "Alphabet Letter to Letter",
     columns: [
       {
-        Header: "Question",
-        accessor: "aa",
+        Header: "English Question",
+        accessor: "engQuestion",
+        type: "string",
+      },
+      {
+        Header: "Hindi Question",
+        accessor: "hinQuestion",
+        type: "string",
+      },
+      {
+        Header: "Options",
+        accessor: "options",
+        type: "array",
+        Cell: ({ value }) => value.join(", "),
+      },
+      {
+        Header: "Answers",
+        accessor: "answers",
+        type: "array",
+        Cell: ({ value }) => value.join(", "),
+      },
+      {
+        Header: "University",
+        accessor: "University.name",
+        type: "string",
+      },
+      {
+        Header: "Subject",
+        accessor: "Subject.name",
+        type: "string",
+      },
+      {
+        Header: "Grade",
+        accessor: "Grade.name",
         type: "string",
       },
     ],
     importSampleFile:
       "https://docs.google.com/spreadsheets/d/1m7phWjHexIXj64lILs9yXUS6CilrqAKD23TVBrUM5dc/edit?gid=0#gid=0",
     icon: "rule",
+    pagination: {
+      totalRecords: 21,
+      currentPage: 5,
+      totalPages: 5,
+    },
+    data: [],
   },
 };
-
 
 const renderJsonCell = (value, type = "default") => {
   if (!value) return "-";
